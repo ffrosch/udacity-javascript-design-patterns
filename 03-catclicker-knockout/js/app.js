25b1f91091292abb6263a4442b70248b3fbecacc
Florian Frosch <ffrosch.dev@posteo.de>
2022-09-29 18:03:26 +0200

    feat: support multiple cats

--- 03-catclicker-knockout/js/app.js
+++ 03-catclicker-knockout/js/app.js
@@ -1,15 +1,48 @@
-const Cat = function () {
-    this.nicknames = ko.observableArray([
-        "Cutie-Pie",
-        "Wesserson",
-        "Nic-Nac",
-        "PinScar",
-    ]);
+var model = [
+    {
+        clickCount: 0,
+        name: 'Tabby',
+        imgSrc: 'img/434164568_fea0ad4013_z.jpg',
+        imgAttribution: 'https://www.flickr.com/photos/bigtallguy/434164568',
+        nicknames: ["Tabea"]
+    },
+    {
+        clickCount: 0,
+        name: 'Tiger',
+        imgSrc: 'img/4154543904_6e2428c421_z.jpg',
+        imgAttribution: 'https://www.flickr.com/photos/xshamx/4154543904',
+        nicknames: ["Tigger"]
+    },
+    {
+        clickCount: 0,
+        name: 'Scaredy',
+        imgSrc: 'img/22252709_010df3379e_z.jpg',
+        imgAttribution: 'https://www.flickr.com/photos/kpjas/22252709',
+        nicknames: ["Hop-And-Gone"]
+    },
+    {
+        clickCount: 0,
+        name: 'Shadow',
+        imgSrc: 'img/1413379559_412a540d29_z.jpg',
+        imgAttribution: 'https://www.flickr.com/photos/malfet/1413379559',
+        nicknames: ["Hidey"]
+    },
+    {
+        clickCount: 0,
+        name: 'Sleepy',
+        imgSrc: 'img/9648464288_2516b35537_z.jpg',
+        imgAttribution: 'https://www.flickr.com/photos/onesharp/9648464288',
+        nicknames: ["Smoochy"]
+    }
+]
 
-    this.clickCount = ko.observable(0);
-    this.name = ko.observable("Tabby");
-    this.imgSrc = ko.observable("img/22252709_010df3379e_z.jpg");
-    this.imgAttribution = ko.observable("source?");
+
+const Cat = function (data) {
+    this.nicknames = ko.observableArray(data.nicknames);
+    this.clickCount = ko.observable(data.clickCount);
+    this.name = ko.observable(data.name);
+    this.imgSrc = ko.observable(data.imgSrc);
+    this.imgAttribution = ko.observable(data.imgAttribution);
 
     this.level = ko.computed(function () {
         let title;
@@ -31,14 +64,19 @@ const Cat = function () {
     }, this);
 };
 
-const ViewModel = function () {
+const ViewModel = function (data) {
     const self = this;
+    this.catList = ko.observableArray([]);
 
-    this.currentCat = ko.observable(new Cat());
+    data.forEach(function (catItem) {
+        self.catList.push(new Cat(catItem));
+    });
+
+    this.currentCat = ko.observable(this.catList()[0]);
 
     this.incrementCounter = function () {
         self.currentCat().clickCount(self.currentCat().clickCount() + 1);
-    }
+    };
 };
 
-ko.applyBindings(new ViewModel());
+ko.applyBindings(new ViewModel(model));
