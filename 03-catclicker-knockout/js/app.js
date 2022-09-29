4e932eb7ce056dc810ff16bd0f96ee8e2c6a69c2
Florian Frosch <ffrosch.dev@posteo.de>
2022-09-29 17:50:45 +0200

    feat: with-context

--- 03-catclicker-knockout/js/app.js
+++ 03-catclicker-knockout/js/app.js
@@ -1,27 +1,44 @@
-const ViewModel = function () {
+const Cat = function () {
+    this.nicknames = ko.observableArray([
+        "Cutie-Pie",
+        "Wesserson",
+        "Nic-Nac",
+        "PinScar",
+    ]);
 
     this.clickCount = ko.observable(0);
     this.name = ko.observable("Tabby");
     this.imgSrc = ko.observable("img/22252709_010df3379e_z.jpg");
     this.imgAttribution = ko.observable("source?");
 
-    this.incrementCounter = function () {
-        this.clickCount(this.clickCount() + 1);
-    }
-
     this.level = ko.computed(function () {
+        let title;
         const cnt = this.clickCount();
+
         if (cnt < 10) {
-            return "baby";
+            title = "Baby";
         } else if (cnt < 50) {
-            return "infant";
+            title = "Infant";
         } else if (cnt < 100) {
-            return "teen";
+            title = "Teen";
+        } else if (cnt < 500) {
+            title = "Adult";
         } else {
-            console.log(cnt);
-            return "adult";
+            title = "Ninja";
         }
+
+        return title;
     }, this);
+};
+
+const ViewModel = function () {
+    const self = this;
+
+    this.currentCat = ko.observable(new Cat());
+
+    this.incrementCounter = function () {
+        self.currentCat().clickCount(self.currentCat().clickCount() + 1);
+    }
 };
 
 ko.applyBindings(new ViewModel());
